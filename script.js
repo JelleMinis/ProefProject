f229edda072778bc71acddc7602cf2dac5d686fc
DevelopCouch <39366350+DevelopCouch@users.noreply.github.com>
2023-03-26 16:23:07 +0200

    aanpassing

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
 "use strict";
 
 //Declaratie van const
-const headerInput = document.querySelector(".inputHeader");
-const textInput = document.querySelector(".inputArea");
+const h1EL = document.querySelector(".text-H1");
+const textInputEL = document.querySelector(".textArea");
 const btnInput = document.querySelector(".btn--input");
 
+//string fixer
+const stringFixer = function (str) {};
+
+//string input
+
+//Header input
+
 const addElement = function () {
-  const headerInput = document.querySelector(".inputHeader");
-  const textInput = document.querySelector(".inputArea");
+  const h1 = h1EL.value + " ";
+  const text = textInputEL.value;
+  console.log(text);
 
   // create a new div element
   const newSection = document.createElement("section");
 
   // and give it some content
-  const headerContent = document.createTextNode(headerInput);
-  const newContent = document.createTextNode(textInput);
+  const headerContent = document.createTextNode(h1);
+  const newContent = document.createTextNode(text);
 
-  // add the text node to the newly created div
   newSection.appendChild(headerContent);
+  newSection.appendChild(newContent);
 
   // add the newly created element and its content into the DOM
   const currentSection = document.getElementById("div1");
   document.body.insertBefore(newSection, currentSection);
 };
 
-console.log(btnInput);
+// console.log(btnInput);
 btnInput.addEventListener("click", addElement);
+
+// const squareDigits = function (num) {
+//   const a = Array.from(String(num), Number);
+//   console.log(a);
+//   let b = [];
+//   for (let i = 0; i < a.length; i++) {
+//     const c = Math.pow(a[i], 2);
+//     b.push(c);
+//   }
+//   return Number(b.join(""));
+// };
+
+// const printerError = function (str) {
+//   let numError = 0;
+//   const alph = "abcdefghijklmnopqrstuwvxyz";
+//   const error = [...alph.slice(13, alph.length)];
+//   for (const i = 0; i < str.length; i++) {}
+//   return console.log(error);
+// };
+
+// console.log(printerError("aaaaaaaadddddddddddddzzzz"));
